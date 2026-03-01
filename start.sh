6729e17588f44d04c941f7fe60ce46b19968d35e
Shane <shane.pennicott@gmail.com>
2026-03-01 11:14:21 +0000

    Update

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
-source .venv/Scripts/activate
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
 python3 -m app.app
 deactivate
